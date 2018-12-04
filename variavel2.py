78875d506d63cc5777ccdca8bcd1b43199025c8f
edybahia <edeilsonbahia@gmail.com>
2018-12-03 21:34:46 -0300

    Enviando

--- variavel2.py
+++ variavel2.py
@@ -1,5 +1,5 @@
 
 nome='edy'
-nome2= 'edson
+nome2= 'edson'
 # caso fosse no shell não ia precisar do print, pois o shell entende que é para exibir
 print(nome + ' ' + nome2)
---
+++ variavel3.py
@@ -0,0 +1,4 @@
+nome= 'fulano de tal' # por ser ua string posso add qualquer frase
+print(nome[2])  #Forma de pegar uma letra no vetor especifico
+print(nome[-1]) # no -1 ele pega sempre a ultima letra não impota o tamanho
+print(len(nome)) #retorna o tamando do vetor
