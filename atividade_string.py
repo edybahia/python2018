53be5e4fc4ce88f71c28e352f9dbf661fa486bf0
edybahia <edeilsonbahia@gmail.com>
2018-12-03 21:55:03 -0300

    Enviando

---
+++ atividade_string.py
@@ -0,0 +1,10 @@
+nome = 'edson'
+sobrenome_mae= 'brito'
+sobrenome_pai = 'santos'
+
+#usando os indice de vetor para exibir as palavras neste exemploa  saída do console deve ser um ebs
+
+print(nome[0]+sobrenome_mae[0]+sobrenome_pai[0])
+print('e-mail criado: '+ nome +'.'+sobrenome_mae+'@gmail.com')
+
+# O sinal de mais apenas realiza a concatenização das palavras
---
+++ numeros.py
@@ -0,0 +1,6 @@
+# Nesta opção o python retorna um true or false para represetnar se é maior ou não
+# neste exemplo ele vai apresentar true, pois 5 é maior
+print(5>2, '5 é maior que 2')
+print(5!=2, '5 é diferente que 2')
+print(5==2, ' 5 é igual a 2')
+
