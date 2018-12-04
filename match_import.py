88d40ce3e0f680cef3f4615c2240ad919f273e24
edybahia <edeilsonbahia@gmail.com>
2018-12-03 22:22:01 -0300

    Enviando

---
+++ match_import.py
@@ -0,0 +1,9 @@
+import math
+
+# Necessário realizar a declaração para float devolver o valor
+# Cuidado na concatenização com os sinais de + por exemplo se não colocar o tipo de variavel
+# dar erro
+
+valor01 = float (input('Numero para calcular a raiz'))
+print(math.sqrt(valor01))
+
