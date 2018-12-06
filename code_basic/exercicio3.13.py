137ef748eda724aadeb862e4da9d6dd9afb9b735
edybahia <edeilsonbahia@gmail.com>
2018-12-05 22:55:08 -0300

    Commit

---
+++ code_basic/exercicio3.13.py
@@ -0,0 +1,5 @@
+# Escreva um programa para converter temperatura em celsos para Farenheit
+
+tempc= int (input('Digite o valor da temperatura em ºC'))
+tempf= ((9 * tempc)/ 5)+ 32
+print("O valor da temperatura em F é %3.3f " %tempf )
---
+++ code_basic/exercicio3.7.py
@@ -0,0 +1,5 @@
+#escrever um programa que peça dois numeros inteiros. Imprima a soma desses dois números na tela
+
+valor01= int (input('Digite um número para soma'))
+valor02= int (input('Digite um segundo número para soma'))
+print ('O valor da soma de %d + %d é igual a %3.3f' %(valor01, valor02, valor01+valor02))
