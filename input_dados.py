15dd58fe83e37760d6bcbcb1d5be5f018a8e2eef
edybahia <edeilsonbahia@gmail.com>
2019-01-17 14:30:07 -0300

    Commit

--- input_dados.py
+++ input_dados.py
@@ -4,6 +4,8 @@ import math
 # Cuidado na concatenização com os sinais de + por exemplo se não colocar o tipo de variavel
 # dar erro
 
+# No python a variavel Valor01, vai receber o valor digitado pelo usuário. Lembrar que aqui o comando input pode escrever
+
 valor01 = float (input('Numero para calcular a raiz'))
 print(math.sqrt(valor01))
 
